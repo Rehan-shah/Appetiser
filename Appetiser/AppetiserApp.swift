ccad0d217b828edc99636884017b2e219a57bd42
Rehan-shah <Rehansnehalshah1@gmail.com>
2023-11-19 17:12:29 +0530

    Initial Commit

---
+++ Appetiser/AppetiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetiserApp.swift
+//  Appetiser
+//
+//  Created by Rehan Shah on 19/11/23.
+//
+
+import SwiftUI
+
+@main
+struct AppetiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
